92e0945bc447d56b41b1df4558273cba257fc0d3
Carlos Gonzalez <cvrlosgonzalez@gmail.com>
2016-05-23 10:10:14 -0400

    about to pull

--- db/schema.rb
+++ db/schema.rb
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20160520164513) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "pnumber"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
