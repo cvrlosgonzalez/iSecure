f7d57d302316aa37fc0eff976a47892170d4f948
Carlos Gonzalez <cvrlosgonzalez@gmail.com>
2016-05-23 20:09:43 -0400

    added background color, header dropdown for cameras and account settings

--- db/migrate/20160516212600_add_monitoring_and_api_name_to_cams.rb
+++
@@ -1,6 +0,0 @@
-class AddMonitoringAndApiNameToCams < ActiveRecord::Migration
-  def change
-    add_column :cams, :monitoring, :boolean
-    add_column :cams, :api_name, :string
-  end
-end
--- db/migrate/20160520164513_add_text_alerts_to_cams.rb
+++
@@ -1,5 +0,0 @@
-class AddTextAlertsToCams < ActiveRecord::Migration
-  def change
-    add_column :cams, :text_alerts, :boolean
-  end
-end
