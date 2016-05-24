d3543bbe1951879fdfc4ae9f01b664d39224dfcf
Carlos Gonzalez <cvrlosgonzalez@gmail.com>
2016-05-24 13:02:05 -0400

    fixed logo

--- db/migrate/20160516210010_add_cam_id_to_alerts.rb
+++
@@ -1,5 +0,0 @@
-class AddCamIdToAlerts < ActiveRecord::Migration
-  def change
-    add_column :alerts, :cam_id, :integer
-  end
-end
--- db/migrate/20160519134655_create_text_messages.rb
+++
@@ -1,8 +0,0 @@
-class CreateTextMessages < ActiveRecord::Migration
-  def change
-    create_table :text_messages do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
